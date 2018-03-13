d657aa48e0f4c2e5c46f4fad53827531765a87be
tiantiio <tiantidaili@gmail.com>
2018-03-13 13:11:19 +0800

    m

--- op/get-https.sh
+++ op/get-https.sh
@@ -1,1 +1,1 @@
-docker run -v /root/nav/op/letsencrypt_data:/etc/letsencrypt beego /opt/letsencrypt/certbot-auto certonly --standalone -d v2nav.com -d www.v2nav.com --agree-tos -n -m microcpa@gmail.com
+docker run -v /root/nav/op/letsencrypt_data:/etc/letsencrypt -p 80:80 -p 443:443 op_beego /opt/letsencrypt/certbot-auto certonly --standalone -d v2nav.com -d www.v2nav.com --agree-tos -n -m microcpa@gmail.com
